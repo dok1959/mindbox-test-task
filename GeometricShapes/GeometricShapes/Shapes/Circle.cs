f923f920eb2053481cea30b686f241eca90b2186
dok1959 <dok1959.good@mail.ru>
2024-06-09 14:58:28 +1000

    Добавлена фигура круг

---
+++ GeometricShapes/GeometricShapes/Shapes/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace GeometricShapes.Shapes
+{
+    /// <summary>
+    /// Круг
+    /// </summary>
+    public class Circle : IShape
+    {
+        private readonly double _radius;
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        /// <summary>
+        /// Получить площадь
+        /// </summary>
+        public double GetArea() => Math.PI * Math.Pow(2, _radius);
+    }
+}
