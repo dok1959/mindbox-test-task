971f459088faf98102f1c26eab4ba7f43f0e242a
dok1959 <dok1959.good@mail.ru>
2024-06-10 01:12:54 +1000

    Добавлен тест на проверку прямоугольности

---
+++ GeometricShapes/GeometricShapes.Tests/Unit/TriangleIsRightTests.cs
@@ -0,0 +1,27 @@
+﻿using GeometricShapes.Core.Shapes;
+
+namespace GeometricShapes.Tests.Unit
+{
+    public class TriangleIsRightTests
+    {
+        [Fact]
+        public void IsRight_Valid()
+        {
+            var triangle = new Triangle(3, 4, 5);
+
+            var result = triangle.IsRight();
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsRight_NotValid_WrongSides()
+        {
+            var triangle = new Triangle(4, 4, 5);
+
+            var result = triangle.IsRight();
+
+            Assert.False(result);
+        }
+    }
+}
