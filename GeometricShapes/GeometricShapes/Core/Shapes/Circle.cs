c22467602280c25889542c0f8c40dc948853da62
dok1959 <dok1959.good@mail.ru>
2024-06-09 23:58:34 +1000

    Добавлено использование валидаторов при создании фигуры

---
+++ GeometricShapes/GeometricShapes/Core/Shapes/Circle.cs
@@ -0,0 +1,27 @@
+﻿using GeometricShapes.Core.Validators;
+
+namespace GeometricShapes.Core.Shapes
+{
+    /// <summary>
+    /// Круг
+    /// </summary>
+    public class Circle : IShape
+    {
+        private readonly double _radius;
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="radius">Радиус</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public Circle(double radius)
+        {
+            CircleValidator.Validate(radius);
+
+            _radius = radius;
+        }
+
+        /// <inheritdoc />
+        public double GetArea() => Math.PI * Math.Pow(2, _radius);
+    }
+}
---
+++ GeometricShapes/GeometricShapes/Core/Shapes/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using GeometricShapes.Core.Validators;
+
+namespace GeometricShapes.Core.Shapes
+{
+    /// <summary>
+    /// Треугольник
+    /// </summary>
+    public class Triangle : IShape
+    {
+        private readonly double _firstSide;
+
+        private readonly double _secondSide;
+
+        private readonly double _thirdSide;
+
+        /// <summary>
+        /// .ctor
+        /// </summary>
+        /// <param name="firstSide">Первая сторона</param>
+        /// <param name="secondSide">Вторая сторона</param>
+        /// <param name="thirdSide">Третья сторона</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public Triangle(double firstSide, double secondSide, double thirdSide)
+        {
+            TriangleValidator.Validate(firstSide, secondSide, thirdSide);
+
+            _firstSide = firstSide;
+            _secondSide = secondSide;
+            _thirdSide = thirdSide;
+        }
+
+        /// <inheritdoc />
+        public double GetArea()
+        {
+            var semiPerimeter = (_firstSide + _secondSide + _thirdSide) / 2;
+
+            return Math.Sqrt(semiPerimeter
+                * (semiPerimeter - _firstSide)
+                * (semiPerimeter - _secondSide)
+                * (semiPerimeter - _thirdSide));
+        }
+    }
+}
