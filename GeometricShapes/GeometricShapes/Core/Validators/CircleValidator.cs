024dd19cdb79fe419997f1abcca34cfa2b8653d1
dok1959 <dok1959.good@mail.ru>
2024-06-09 23:57:29 +1000

    Добавлены классы для валидации фигур

---
+++ GeometricShapes/GeometricShapes/Core/Validators/CircleValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace GeometricShapes.Core.Validators
+{
+    /// <summary>
+    /// Валидатор круга
+    /// </summary>
+    public class CircleValidator
+    {
+        /// <summary>
+        /// Проверить корректность радиуса круга
+        /// </summary>
+        /// <param name="radius">Первая сторона</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static void Validate(double radius)
+        {
+            if (radius <= 0)
+                throw new ArgumentOutOfRangeException(nameof(radius), CoreValidationErrors.WrongCircleRadiusValue);
+        }
+    }
+}
---
+++ GeometricShapes/GeometricShapes/Core/Validators/TriangleValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace GeometricShapes.Core.Validators
+{
+    /// <summary>
+    /// Валидатор треугольника
+    /// </summary>
+    public static class TriangleValidator
+    {
+        /// <summary>
+        /// Проверить корректность сторон треугольника
+        /// </summary>
+        /// <param name="firstSide">Первая сторона</param>
+        /// <param name="secondSide">Вторая сторона</param>
+        /// <param name="thirdSide">Третья сторона</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static void Validate(double firstSide, double secondSide, double thirdSide)
+        {
+            if (firstSide <= 0 || secondSide <= 0 || thirdSide <= 0)
+                throw new ArgumentOutOfRangeException(nameof(firstSide), CoreValidationErrors.WrongTriangleSideValue);
+
+            var isTriangleInequal = IsTriangleInequal(firstSide, secondSide, thirdSide);
+            if (!isTriangleInequal)
+                throw new ArgumentException(CoreValidationErrors.WrongTriangleInequality);
+        }
+
+        private static bool IsTriangleInequal(double firstSide, double secondSide, double thirdSide)
+        {
+            return firstSide < (secondSide + thirdSide)
+                && secondSide < (firstSide + thirdSide)
+                && thirdSide < (firstSide + secondSide);
+        }
+    }
+}
