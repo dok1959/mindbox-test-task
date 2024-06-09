c4e21ed490a4b48f2e6667ec122af5936e051fa5
dok1959 <dok1959.good@mail.ru>
2024-06-09 14:58:08 +1000

    Добавлен контракт фигуры

---
+++ GeometricShapes/GeometricShapes/Shapes/IShape.cs
@@ -0,0 +1,13 @@
+﻿namespace GeometricShapes.Shapes
+{
+    /// <summary>
+    /// Фигура
+    /// </summary>
+    internal interface IShape
+    {
+        /// <summary>
+        /// Получить площадь фигуры
+        /// </summary>
+        double GetArea();
+    }
+}
