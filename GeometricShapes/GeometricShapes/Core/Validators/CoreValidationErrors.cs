29da32ae47a56eded31675fba279e0ee32f55966
dok1959 <dok1959.good@mail.ru>
2024-06-09 23:57:13 +1000

    Добавлен отдельный класс для ошибок валидации

---
+++ GeometricShapes/GeometricShapes/Core/Validators/CoreValidationErrors.cs
@@ -0,0 +1,14 @@
+﻿namespace GeometricShapes.Core.Validators
+{
+    /// <summary>
+    /// Сообщения об ошибках валидации фигур
+    /// </summary>
+    public class CoreValidationErrors
+    {
+        public const string WrongTriangleSideValue = "Сторона треугольника не может быть меньше или равна нулю";
+
+        public const string WrongTriangleInequality = "Длина любой стороны треугольника не может быть больше суммы длин двух других сторон";
+
+        public const string WrongCircleRadiusValue = "Радиус круга не может быть меньше либо равен нулю";
+    }
+}
